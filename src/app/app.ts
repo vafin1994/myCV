e46fed4bd4540a177a987a2ec7acb1b3b6ceddd9
Oleksandr Vafin <vafinalex@gmail.com>
2026-06-04 21:29:52 +0200

    Feature/my cv#1 basic web page (#10)
    * myCV#1 Add resume.json file, get it and display
    
    * myCV#1 Fix resume.json according to schema

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit, signal, WritableSignal} from '@angular/core';
+import {ResumeService} from './services/resume-service';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    JsonPipe
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  private resumeService = inject(ResumeService);
+  public resume: WritableSignal<JSON | null> = signal(null);
+
   protected readonly title = signal('myCV');
+
+  ngOnInit() {
+    this.getResumeJson();
+  }
+
+  private getResumeJson() {
+    this.resumeService.getResume().subscribe(
+      resume => {
+        this.resume.set(resume);
+      }
+    )
+  }
 }
