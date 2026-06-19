5a3a9da773d67247e6a0f6f2d662c5d3ef9fed30
oleksandr.vafin <vafinalex@gmail.com>
2026-06-19 21:39:16 +0200

    fix resume-service.ts

--- src/app/services/resume-service.ts
+++ src/app/services/resume-service.ts
@@ -1,14 +1,15 @@
-import {inject, Service } from '@angular/core';
+import {inject, Service} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Resume} from '../models/resume.interface';
 
-
 @Service()
 export class ResumeService {
   private readonly http = inject(HttpClient);
+  private readonly document = inject(DOCUMENT);
 
   public getResume(): Observable<Resume> {
-    return this.http.get<Resume>('/resources/resume.json');
+    return this.http.get<Resume>(this.document.baseURI + 'resources/resume.json');
   }
 }
